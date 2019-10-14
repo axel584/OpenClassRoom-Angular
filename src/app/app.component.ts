1d7803ec022c10df8eedba56d9d98b6492faa1ab
Rousseau Axel <axel.rousseau@brgm.fr>
2019-10-14 17:00:19 +0200

    correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import * as firebase from 'firebase';
-import {Post} from './post';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +10,6 @@ export class AppComponent {
 
 
   
-  constructor() {
-    var firebaseConfig = {
-      apiKey: "AIzaSyBpKgf3tjEPgTFPxk4k5wQZkyEo-xSKDbQ",
-      authDomain: "http-client-demo-97bd3.firebaseapp.com",
-      databaseURL: "https://http-client-demo-97bd3.firebaseio.com",
-      projectId: "http-client-demo-97bd3",
-      storageBucket: "http-client-demo-97bd3.appspot.com",
-      messagingSenderId: "943185286554",
-      appId: "1:943185286554:web:4b29a9c8ffb450a6"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-  }
+  constructor() {}
 
 }
